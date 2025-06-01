4b0580e5404877025ab0798d99a80e3ae073019f
Seu Nome <seuemail@example.com>
2025-06-01 12:38:18 -0300

    iniciando projeto

--- src/main/java/equipe/hotwheels/clinica_medica/repository/AgendaRepository.java
+++
@@ -1,7 +0,0 @@
-package equipe.hotwheels.clinica_medica.repository;
-
-import equipe.hotwheels.clinica_medica.model.Agenda;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AgendaRepository extends JpaRepository<Agenda, Long> {
-}
--- src/main/java/equipe/hotwheels/clinica_medica/repository/PacienteRepository.java
+++
@@ -1,7 +0,0 @@
-package equipe.hotwheels.clinica_medica.repository;
-
-import equipe.hotwheels.clinica_medica.model.Paciente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PacienteRepository extends JpaRepository<Paciente, Long> {
-}
