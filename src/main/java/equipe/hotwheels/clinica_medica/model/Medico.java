94c80fc8c15d5e142bc86b6a6e1b6598b4261292
Seu Nome <seuemail@example.com>
2025-06-01 17:45:31 -0300

    criando a controler de medico e a model

---
+++ src/main/java/equipe/hotwheels/clinica_medica/model/Medico.java
@@ -0,0 +1,47 @@
+package equipe.hotwheels.clinica_medica.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String crm;
+    private String especialidade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+}
